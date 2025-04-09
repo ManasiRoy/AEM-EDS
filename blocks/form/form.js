5c5fbc31d7c8434ba38c00ef03d8d232acba8ab3
Front End Developer <manasiry3@gmail.com>
2025-04-09 14:30:00 +0530

    block added accordion and form

---
+++ blocks/form/form.js
@@ -0,0 +1,32 @@
+export default function decorate(block) {
+    const form = document.createElement('form');
+    console.log('✅ Form block loaded');
+    [...block.children].forEach((row) => {
+        const labelText = row.children[0]?.textContent.trim().toLowerCase();
+        const value = row.children[1]?.textContent.trim();
+
+        if (labelText === 'label') {
+            const label = document.createElement('label');
+            label.textContent = value;
+
+            const input = document.createElement('input');
+            input.name = value.toLowerCase().replace(/\s+/g, '-');
+
+            form.appendChild(label);
+            form.appendChild(document.createElement('br'));
+            form.appendChild(input);
+            form.appendChild(document.createElement('br'));
+        }
+
+        if (labelText === 'submit') {
+            const button = document.createElement('button');
+            button.type = 'submit';
+            button.textContent = value;
+            form.appendChild(document.createElement('br'));
+            form.appendChild(button);
+        }
+    });
+
+    block.innerHTML = ''; // clear block content
+    block.appendChild(form);
+}
